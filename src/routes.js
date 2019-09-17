1cd44a7653cd739cacedf050a5aa732a1dd35604
hamdi <hamdigatri92@gmail.com>
2019-09-17 23:48:47 +0100

    Wrapp Route component into Router.

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Route, IndexRoute } from "react-router-dom";
+import { Route, IndexRoute, Router } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Book from "./components/book";
 import App from "./components/app";
+import { createBrowserHistory } from "history";
 
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home}></IndexRoute>
-    <Route path="/about" component={About}></Route>
-    <Route path="/books" component={Book}></Route>
-  </Route>
+const history = createBrowserHistory();
+
+const Routes = (
+  <Router history={history}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}></IndexRoute>
+      <Route path="/about" component={About}></Route>
+      <Route path="/books" component={Book}></Route>
+    </Route>
+  </Router>
 );
+
+export default Routes;
