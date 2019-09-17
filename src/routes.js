956cf4cd8536cdf38701a137a3213be873ca4a3c
hamdi <hamdigatri92@gmail.com>
2019-09-17 23:34:34 +0100

    Fix app component importation.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Route, IndexRoute } from "react-router";
+import { Route, IndexRoute } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Book from "./components/book";
-import App from "./components/App";
+import App from "./components/app";
 
 export default (
   <Route path="/" component={App}>
