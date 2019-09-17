f97a8ac29095dfbec285c52629b340ac2acc72a0
hamdi <hamdigatri92@gmail.com>
2019-09-17 23:48:21 +0100

    Fix app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,27 +1,21 @@
 import React from "react";
-import Link from "react-router-dom";
-import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
 
-const App = props => {
-  console.log("app**");
-  return (
-    <div>
-      <h1>Bookshop</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/book">Book</Link>
-        </li>
-      </ul>
-      <div>{props.children}</div>
-    </div>
-  );
-};
+const App = (
+  <div>
+    <h1>Bookshop</h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/book">Book</Link>
+      </li>
+    </ul>
+  </div>
+);
+
 export default App;
-
-ReactDOM.render(App, document.querySelector("#app"));
